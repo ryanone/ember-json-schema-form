af2932380cef9640cea5dd4eccb0576ff116661a
Ryan Cruz <cruzryan@gmail.com>
2022-06-29 01:23:48 -0700

    Cleanup

--- tests/dummy/app/controllers/application.ts
+++ tests/dummy/app/controllers/application.ts
@@ -1,15 +1,6 @@
 import Controller from '@ember/controller';
-// import { inject as service } from '@ember/service';
-// import FooBarBarService from 'ember-dynamic-form/services/foo-bar-bar';
-// import RegistryService from 'ember-dynamic-form/services/dynamic-form/registry';
 
 export default class ApplicationController extends Controller {
-  // @service('foo-bar-bar')
-  // declare fooBarBar: FooBarBarService;
-
-  // @service('dynamic-form/registry')
-  // declare registry: RegistryService;
-
   get data() {
     return {
       description: 'My description',
@@ -23,13 +14,5 @@ export default class ApplicationController extends Controller {
       description: 'My description',
       type: 'string',
     };
-  }
-
-  name;
-
-  constructor() {
-    super();
-    // this.name = this.fooBarBar;
-    this.name = 'foo';
   }
 }
